d077fc74756270b330f99bf96d835fd30fca44b1
Rafiul Islam Hredoy <64517450+Kaka-Co@users.noreply.github.com>
2023-08-07 11:48:24 +0600

    updated

--- src/main/java/com/rafiul/productsystem/ProductSystemApplication.java
+++ src/main/java/com/rafiul/productsystem/ProductSystemApplication.java
@@ -1,13 +1,27 @@
 package com.rafiul.productsystem;
 
+import com.rafiul.productsystem.model.Product;
+import com.rafiul.productsystem.repository.ProductRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// In this branch we have tried populate data in real database from main method
+ /*modified:   src/main/java/com/rafiul/productsystem/ProductSystemApplication.java
+		 modified:   src/main/java/com/rafiul/productsystem/model/Product.java*/
+
 @SpringBootApplication
-public class ProductSystemApplication {
+@RequiredArgsConstructor
+public class ProductSystemApplication implements CommandLineRunner {
+	private final ProductRepo repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductSystemApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+//		repo.save(Product.builder().name("sample product").build());
+	}
 }
--- src/main/java/com/rafiul/productsystem/model/Product.java
+++ src/main/java/com/rafiul/productsystem/model/Product.java
@@ -3,12 +3,14 @@ package com.rafiul.productsystem.model;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "products")
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
